3c5f18c23cff4e3457187946571ad93268ac7599
HilmiIrfan <hilminaafiudin@gmail.com>
2024-09-18 18:59:53 +0700

    Jobsheet 3

--- Jobsheet_3/Praktikum3.dart
+++ Jobsheet_3/Praktikum3.dart
@@ -1,8 +1,7 @@
 void main(){
-  int Index=0;
-  for(Index==0; Index<20; Index++){
-    if(Index==21) break;
-    else if(Index>1||Index<7) continue;
-    print(Index);
+  for (int index=0; index<=30; index++){
+    if (index == 21) break;
+    else if (index>1&&index<7) continue;
+    print(index);
   }
 }
